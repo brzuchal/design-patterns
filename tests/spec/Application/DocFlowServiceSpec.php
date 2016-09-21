9193187aa2c6d4996c68fb2f6f9a3e52f2990b8c
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2016-09-21 10:34:37 +0200

    Fixed tests

--- tests/spec/Application/DocFlowServiceSpec.php
+++ tests/spec/Application/DocFlowServiceSpec.php
@@ -4,6 +4,7 @@ namespace spec\DocFlow\Application;
 use DocFlow\Application\DocFlowService;
 use DocFlow\Domain\Document\DocumentNumber;
 use DocFlow\Domain\Document\DocumentRepository;
+use DocFlow\Domain\Document\DocumentValidator;
 use DocFlow\Domain\Document\NumberGenerator;
 use DocFlow\Domain\DomainRegistry;
 use DocFlow\Domain\User\AuditUser;
@@ -21,19 +22,30 @@ use Webmozart\Assert\Assert;
  */
 class DocFlowServiceSpec extends ObjectBehavior
 {
-    function let(DomainRegistry $domainRegistry, UserRepository $userRepository, DocumentRepository $documentRepository, NumberGenerator $numberGenerator)
+    function let(
+        DomainRegistry $domainRegistry,
+        UserRepository $userRepository,
+        DocumentRepository $documentRepository,
+        NumberGenerator $numberGenerator,
+        DocumentValidator $documentValidator
+    )
     {
         $domainRegistry->getUserRepository()->willReturn($userRepository);
         $domainRegistry->getDocumentRepository()->willReturn($documentRepository);
-        $this->beConstructedWith(DocFlowService::ENV_PROD, $domainRegistry, $numberGenerator);
+        $this->beConstructedWith(DocFlowService::ENV_PROD, $domainRegistry, $numberGenerator, $documentValidator);
     }
     function it_is_initializable()
     {
         $this->shouldHaveType(DocFlowService::class);
     }
-    function it_uses_DemoNumberGenerator_when_running_on_demo(DomainRegistry $domainRegistry, UserRepository $userRepository, NumberGenerator $numberGenerator)
+    function it_uses_DemoNumberGenerator_when_running_on_demo(
+        DomainRegistry $domainRegistry,
+        UserRepository $userRepository,
+        NumberGenerator $numberGenerator,
+        DocumentValidator $documentValidator
+    )
     {
-        $this->beConstructedWith(DocFlowService::ENV_DEMO, $domainRegistry, $numberGenerator);
+        $this->beConstructedWith(DocFlowService::ENV_DEMO, $domainRegistry, $numberGenerator, $documentValidator);
 
         $user = new User('a');
         $userRepository->findById('a')->willReturn($user);
